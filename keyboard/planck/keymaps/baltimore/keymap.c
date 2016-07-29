8c80ad471ffa1deafd7dc66d0e82b0c0b0681a5c
Thomas Cherry <thomas.cherry@gmail.com>
2016-07-29 05:24:22 -0400

    changing layout to make things more dvorak like, less redundent

--- keyboard/planck/keymaps/baltimore/keymap.c
+++ keyboard/planck/keymaps/baltimore/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,    KC_Q,    KC_W,    KC_E,  KC_R,   KC_T,   KC_Y,  KC_U,    KC_I,     KC_O,    KC_P, KC_BSPC},
   {KC_LCTL,    KC_A,    KC_S,    KC_D,  KC_F,   KC_G,   KC_H,  KC_J,    KC_K,     KC_L, KC_SCLN,  KC_ENT},
   {KC_LSPO,    KC_Z,    KC_X,    KC_C,  KC_V,   KC_B,   KC_N,  KC_M, KC_COMM,   KC_DOT, KC_SLSH, KC_RSFT},
-  {    NAV, KC_LEAD, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_HYPR,KC_RETURN, XXXXXXX, XXXXXXX}
+  {    NAV, KC_LEAD, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_HYPR,KC_RETURN, KC_KP_ENTER, XXXXXXX}
 },//KC_RSFT MT(MOD_LSFT, KC_RPRN), KC_RSPC
 
 /* Colemak
@@ -98,40 +98,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {    NAV, KC_LEAD, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_HYPR, KC_RETURN, XXXXXXX, XXXXXXX}
 },
 
-/* Lower
+/* Lower  |
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |   =  |   [  |   ]  |  -   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |   /  |   \  |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |   /  |   \  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Lead |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      | Lead |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = {
-  {KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC,    KC_AMPR,    KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL},
-  {_______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_UNDS,    KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, S(KC_NUHS), S(KC_NUBS), KC_SLSH, KC_BSLS, _______},
-  {_______, _______, _______, _______, _______, _______, _______,    _______,    KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+[_LOWER] = {/* KC_BSLS KC_PIPE KC_UNDS KC_LCBR KC_RCBR KC_TILD */
+  { KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL},
+  {_______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, _______,  KC_EQL, KC_LBRC, KC_RBRC, KC_MINS},
+  {_______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, _______, KC_SLSH, KC_BSLS, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,    KC_UP, KC_RGHT}
 },
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |      |      |      |      |      |      |   -  |   =  |   [  |   ]  |  \   |
+ * |      |      |      |      |      |      |      | ISO# | ISO~ | ISO/ | ISO| |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |ISO # |ISO / |      |      |      |
+ * |      | Prev | Play | Next | Mute | Vol- | Vol+ |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
-  { KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_DEL},
-  {_______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+  {_______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,       KC_8,    KC_9,       KC_0,  KC_DEL},
+  {_______, _______, _______, _______, _______, _______, _______, KC_NUHS, S(KC_NUHS), KC_NUBS, S(KC_NUBS), _______},
+  {_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______,    _______,   KC_UP,    _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______,    KC_LEFT, KC_DOWN,    KC_RGHT, _______}
 },
 
 /* Navigation layer
