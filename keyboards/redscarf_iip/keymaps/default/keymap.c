0ade22d4df976943df301cdd5f08ffd3de72b369
Thomas Cherry <thomas.cherry@gmail.com>
2017-06-07 06:56:34 -0400

    compiles, no idea if it works yet

--- keyboards/redscarf_iip/keymaps/default/keymap.c
+++ keyboards/redscarf_iip/keymaps/default/keymap.c
@@ -15,17 +15,53 @@
  */
 
 /*
+Red Scarf II+
+Creator: thomas.cherry@gmail.com
+Author: thomas.cherry@gmail.com
+Date: 2017-05-24
 
+Style: Each file should follow the same style conventions
+    * This file: \n{, tab==4
+    * All functions must be documented in javadoc style
 */
 
 #include "redscarf_iip.h"
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+/*  physical keys
+    {K5B, K5C,   K0A, K0B, K0C, K0D, K0E, K0F, K0G, K0H, K0I, K0J, K0K, K0L, K0M, K0N, K0P, NO, NO}, \
+    {K5D, K5E,   K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, K1L, K1M, K1N, K1P, NO, NO}, \
+    {K5F, K5G,   K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N, K2P, NO, NO}, \
+    {K5H, K5I,   K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, K3N, K3P, NO, NO}, \
+    {K5J, K5K,   K4A, K4B, K4C,           K4J,                K4K, K4L, K4H, K4M, K4N, K4P, NO, NO}  \
+
+*/
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[0] =
+/*
+Dvorak: -- NOTE -- default.c will eventually be QWERTY, I just don't type in that so for testing this is it.
+* control instead of caps lock which is gone
+*/
 {
-    [0] = KEYMAP( /* Base */
-    KC_A,  KC_1,  KC_H, \
-    KC_TAB,  KC_SPC   \
-    ),
+    {KC_F1, KC_F2, KC_ESC,  KC_1,     KC_2,      KC_3,   KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_LBRC, KC_RBRC, KC_BSPC,  KC_INT1, KC_HOME},
+    {KC_F3, KC_F4, KC_TAB,  KC_QUOT,  KC_COMM,   KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH, KC_EQL,  KC_BSLS,  KC_NO,   KC_PGUP},
+    {KC_F5, KC_F6, KC_LCTL, KC_A,     KC_O,      KC_E,   KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS, KC_NO,   KC_ENT,   KC_NO,   KC_PGDOWN},
+    {KC_F7, KC_F8, KC_LSFT, KC_GRAVE, KC_SCOLON, KC_Q,   KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z,             KC_RSHIFT,KC_UP,   KC_END},
+    {KC_F9, KC_F10,KC_LCTL, KC_LALT,  KC_LGUI,   KC_SPACE,                                KC_RGUI, KC_FN1,  KC_RALT, KC_RIGHT, KC_DOWN, KC_RIGHT}
+},
+
+[1] =
+{/* Alt keys */
+    {KC_F11, KC_F12, RESET,   KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_DEL,   KC_INT2,  KC_WH_L},
+    {KC_F13, KC_F14, _______, KC_MS_BTN1, KC_MS_U,    KC_MS_BTN2, _______,    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  KC_WH_U},
+    {KC_F15, KC_F16, _______, KC_MS_L,    KC_MS_D,    KC_MS_R,    _______,    _______, KC_H,    KC_J,    KC_K,    KC_L,    _______, _______, _______, _______,  _______,  KC_WH_D},
+    {KC_F17, KC_F18, KC_LSFT, _______,    KC_MS_BTN3, KC_MS_BTN4, KC_MS_BTN5, _______, _______, _______, KC_MUTE, KC_MPLY, _______, _______,          KC_RSHIFT,KC_VOLU,  KC_WH_R},
+    {KC_F19, KC_F20, KC_LCTL, KC_LALT,    _______,    KC_SPACE,                                                            KC_HYPR, KC_FN1,  KC_RALT, KC_MPRV,  KC_VOLD,  KC_MNXT}
+}
+
 };
 
 const uint16_t PROGMEM fn_actions[] =
@@ -63,10 +99,12 @@ void matrix_scan_user(void)
 
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+    return true;
 }
 
-void led_set_user(uint8_t usb_led) {
+void led_set_user(uint8_t usb_led)
+{
 
 }
--- keyboards/redscarf_iip/redscarf_iip.h
+++ keyboards/redscarf_iip/redscarf_iip.h
@@ -22,13 +22,46 @@
 // The following is an example using the Planck MIT layout
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
-#define KEYMAP( \
+#define _KEYMAP( \
     k00, k01, k02, \
       k10,  k11   \
 ) \
 { \
     { k00, k01,   k02 }, \
     { k10, KC_NO, k11 }, \
+}
+
+#define XXX KC_NO
+
+/* RedScarfII keymap definition macro
+ * K3B and K2M are extra keys for ISO
+
+#define MATRIX_ROW_PINS { B2, B1, B0}
+#define MATRIX_COL_PINS { F4,F1,F0,B3,D0,D1,D4,D5,D6,D7,F7,F6,D2,D3,B6,B5,B4,F5 
+
+*/
+    /*
+           f4  f1 f0 B3 D0 D1 D4 D5 D6 D7 F7 F6 D2 D3 B6 B5 B4 F5
+    f1 f2  esc 1  2  3  4  5  6  7  8  9  0  -  +  bs hm x  x  x
+    f3 f4  t   q  w  e  r  t  y  u  i  o  p  [  ]  \  pu 8  9  x
+    f5 f6  cl  a  s  d  f  g  h  j  k  l  ;  '  i1 rt pd 5  6  x
+    f7 f8  s   i2 z  x  c  v  b  n  m  .  ,  /  rs up ed 2  3  x
+    f9 f10 c1  c2 c3                sp c4 c5 c6 lt dn rt x  x  x
+               f1 f2 f3 f4 f5 f6 f7 f8 f9 f10
+    */
+    //          f4   f1   f0   B3   D0   D1   D4   D5   D6   D7   F7   F6   D2   D3   B6   B5   B4   F5
+#define KEYMAP( \
+    K5B, K5C,   K0A, K0B, K0C, K0D, K0E, K0F, K0G, K0H, K0I, K0J, K0K, K0L, K0M, K0N, K0P, K0Q, K0R, K0S, \
+    K5D, K5E,   K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, K1L, K1M, K1N, K1P, K1Q, K1R, K1S, \
+    K5F, K5G,   K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N, K2P, K2Q, K2R, K2S, \
+    K5H, K5I,   K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, K3N, K3P, K3Q, K3R, K3S, \
+    K5J, K5K,   K4A, K4B, K4C,           K4J,                K4K, K4L, K4H, K4M, K4N, K4P, K4Q, K4R, K4S  \
+) { \
+    {K5B, K5C,   K0A, K0B, K0C, K0D, K0E, K0F, K0G, K0H, K0I, K0J, K0K, K0L, K0M, K0N, K0P, NO, NO,  NO}, \
+    {K5D, K5E,   K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, K1L, K1M, K1N, K1P, NO, NO,  NO}, \
+    {K5F, K5G,   K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N, K2P, NO, NO,  NO}, \
+    {K5H, K5I,   K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, K3N, K3P, NO, NO,  NO}, \
+    {K5J, K5K,   K4A, K4B, K4C,           K4J,                K4K, K4L, K4H, K4M, K4N, K4P, NO, NO,  NO}  \
 }
 
 #endif
